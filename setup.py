56cc58e63e84203d0375e04ad6e2364abd067cbe
Anselm Kruis <a.kruis@science-computing.de>
2011-04-06 18:37:53 +0200

    Bump the version to 0.0.2

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ except ImportError:
 
 setup(
     name='sPickle',
-    version='0.0.1',
+    version='0.0.2',
     description='Extended Pickler for Stackless Python',
     author='Anselm Kruis',
     author_email='a.kruis@science-computing.de',
